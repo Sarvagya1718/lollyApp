e3cbd0ff66fbe59a44e521cf26cd66c7baa5ce7a
Sarvagya1718 <66436470+Sarvagya1718@users.noreply.github.com>
2023-03-08 03:42:54 +0530

    updated with debounce time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,3 +64,79 @@ export class AppComponent implements OnInit {
     }, 500);
   }
 }
+
+
+
+////////////WITH RXJS-debounce time///////////////
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { FormGroup, FormControl } from '@angular/forms';
+// import { debounceTime } from 'rxjs';
+// import { DataService } from './data.service';
+// // import { FormGroup } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss'],
+// })
+// export class AppComponent implements OnInit {
+//   title = 'lollyApp';
+//   data: any = [];
+//   page = 1;
+//   prevVal: any = null;
+//   searchLoad = false;
+//   scrollLoad = false;
+
+//   constructor(private dataservice: DataService) {}
+//   searchForm = new FormGroup({
+//     searchvalue: new FormControl(''),
+//   });
+
+//   url: String | undefined;
+//   ngOnInit() {
+//     this.search();
+//   }
+//   searchval: any;
+//   async search() {
+//     this.searchForm
+//       .get('searchvalue')
+//       ?.valueChanges.pipe(debounceTime(2000))
+
+//       .subscribe(async (res) => {
+//         this.searchval = res;
+//       });
+
+//     if (this.prevVal == null || this.prevVal != this.searchval) {
+//       this.page = 1;
+//       this.searchLoad = true;
+//       this.dataservice
+//         .getSearchData(this.searchval, this.page)
+//         .subscribe(async (res: any) => {
+//           this.data = await res.items ? res.items : res;
+//           this.searchLoad = false;
+//         });
+//       this.prevVal = this.searchval;
+//     }
+//   }
+
+//   onScroll() {
+//     this.dataservice
+//       .getSearchData(this.searchval, ++this.page)
+//       .subscribe((res: any) => {
+//         if (res.items) {
+//           res.items.forEach((item: any) => {
+//             this.scrollLoad = true;
+//             this.data.push(item);
+//           });
+//         } else {
+//           res.forEach((item: any) => {
+//             this.scrollLoad = true;
+//             this.data.push(item);
+//           });
+//         }
+//       });
+//     this.scrollLoad = false;
+//   }
+// }
